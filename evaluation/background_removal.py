1110c88efd2b1e3c1062679fe3973c0b12e44331
PavlosMak <pavlosmakridis1322001@gmail.com>
2024-05-06 16:56:43 +0200

    Add LPIPS

--- evaluation/background_removal.py
+++ evaluation/background_removal.py
@@ -16,11 +16,6 @@ if __name__ == "__main__":
         for file in files:
             output_file = f"{output_paths[i]}/{file}"
             img = np.array(Image.open(f"{p}/{file}"))
-            # black_pixels = np.where(
-            #     (img[:, :, 0] == 0) &
-            #     (img[:, :, 1] == 0) &
-            #     (img[:, :, 2] == 0)
-            # )
             threshold = 100
             black_pixels = np.where(
                 (img[:, :, 0] <= threshold) &
--- evaluation/image_metrics.py
+++ evaluation/image_metrics.py
@@ -2,7 +2,10 @@ import cv2
 import numpy as np
 
 from skimage.metrics import structural_similarity as ssim
+import lpips
 import json
+import torch
+from torchvision.transforms import Normalize
 
 ground_truth = ["/media/pavlos/One Touch/datasets/our_baseline/torus/white/0"]
 ours = ["/home/pavlos/Desktop/stuff/Uni-Masters/thesis/results/our_baseline/ours/torus/test_cam_0_white"]
@@ -10,6 +13,8 @@ pacnerfs = ["/home/pavlos/Desktop/stuff/Uni-Masters/thesis/results/our_baseline/
 results = ["/home/pavlos/Desktop/stuff/Uni-Masters/thesis/results/our_baseline/torus"]
 output_dir = "/home/pavlos/Desktop/stuff/Uni-Masters/thesis/results"
 frames = [20]
+
+loss_fn_alex = lpips.LPIPS(net='alex')
 
 
 def get_psnrs(gt, our, pac):
@@ -22,6 +27,21 @@ def get_ssims(gt, ours, pac):
     ssim_our, ssim_our_diff = ssim(gt, our, full=True, multichannel=True)
     ssim_pac, ssim_pac_diff = ssim(gt, pacnerf, full=True, multichannel=True)
     return ssim_our, ssim_our_diff, ssim_pac, ssim_pac_diff
+
+
+def get_lpips(gt, ours, pac):
+    transform = Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    gt = cv2.cvtColor(gt, cv2.COLOR_BGR2RGB)
+    gt = transform(torch.permute(torch.Tensor(gt), [2, 0, 1]))
+    ours = cv2.cvtColor(ours, cv2.COLOR_BGR2RGB)
+    ours = transform(torch.permute(torch.Tensor(ours), [2, 0, 1]))
+    pac = cv2.cvtColor(pac, cv2.COLOR_BGR2RGB)
+    pac = transform(torch.permute(torch.Tensor(pac), [2, 0, 1]))
+
+    lpips_ours = loss_fn_alex(gt, ours).detach().cpu().item()
+    lpips_pacs = loss_fn_alex(gt, pac).detach().cpu().item()
+
+    return lpips_ours, lpips_pacs
 
 
 if __name__ == "__main__":
@@ -48,9 +68,14 @@ if __name__ == "__main__":
         sequence_results["first_frame_ssim_ours"] = ssim_our
         sequence_results["first_frame_ssim_pac"] = ssim_pac
 
+        lpips_our, lpips_pac = get_lpips(gt, our, pacnerf)
+        sequence_results["first_frame_lpips_ours"] = ssim_our
+        sequence_results["first_frame_lpips_pac"] = ssim_pac
+
         frame_count = frames[i]
         psnr_our, psnr_pac = [], []
         ssim_our, ssim_pac = [], []
+        lpips_our, lpips_pac = [], []
         for fi in range(frame_count):
             gt = f"{sequence}/{str(fi).zfill(6)}.png"  # format 000000
             our = f"{ours[i]}/{fi}.png"
@@ -68,15 +93,23 @@ if __name__ == "__main__":
             ssim_our.append(ssims[0])
             ssim_pac.append(ssims[2])
 
+            lpips = get_lpips(gt, our, pacnerf)
+            lpips_our.append(lpips[0])
+            lpips_pac.append(lpips[1])
+
         psnr_our = np.mean(psnr_our)
         psnr_pac = np.mean(psnr_pac)
         ssim_our = np.mean(ssim_our)
         ssim_pac = np.mean(ssim_pac)
+        lpips_our = np.mean(lpips_our)
+        lpips_pac = np.mean(lpips_pac)
 
         sequence_results["mean_psnr_our"] = psnr_our
         sequence_results["mean_psnr_pac"] = psnr_pac
         sequence_results["mean_ssim_our"] = ssim_our
         sequence_results["mean_ssim_pac"] = ssim_pac
+        sequence_results["mean_lpips_our"] = lpips_our
+        sequence_results["mean_lpips_pac"] = lpips_pac
 
         results.append(sequence_results)
         # TODO: Overlap with the mask?
