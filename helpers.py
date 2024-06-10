Fix masking issue

helpers.py
@@ -151,14 +151,27 @@ def load_scene_data_from_path(path: str, remove_background: bool, seg_as_col=Fal
     is_fg = params['seg_colors'][:, 0] > 0.5
     scene_data = []
     for t in range(len(params['means3D'])):
-        rendervar = {
-            'means3D': params['means3D'][t],
-            'colors_precomp': params['rgb_colors'][t] if not seg_as_col else params['seg_colors'],
-            'rotations': torch.nn.functional.normalize(params['unnorm_rotations'][t]),
-            'opacities': torch.sigmoid(params['logit_opacities']),
-            'scales': torch.exp(params['log_scales']),
-            'means2D': torch.zeros_like(params['means3D'][0], device="cuda")
-        }
+        if "unnorm_rotations" in params and "log_scales" in params:
+            rendervar = {
+                'means3D': params['means3D'][t],
+                'colors_precomp': params['rgb_colors'][t] if not seg_as_col else params['seg_colors'],
+                'rotations': torch.nn.functional.normalize(params['unnorm_rotations'][t]),
+                'opacities': torch.sigmoid(params['logit_opacities']),
+                'scales': torch.exp(params['log_scales']),
+                'means2D': torch.zeros_like(params['means3D'][0], device="cuda")
+            }
+        elif "cov3D_precomp" in params:
+            rendervar = {
+                'means3D': params['means3D'][t],
+                'colors_precomp': params['rgb_colors'][t] if not seg_as_col else params['seg_colors'],
+                # 'rotations': torch.nn.functional.normalize(params['unnorm_rotations'][t]),
+                'opacities': torch.sigmoid(params['logit_opacities']),
+                # 'scales': torch.exp(params['log_scales']),
+                'cov3D_precomp': params['cov3D_precomp'][t],
+                'means2D': torch.zeros_like(params['means3D'][0], device="cuda")
+            }
+        else:
+            raise RuntimeError("Please provide exactly either scale and rotation or precomputed covariances")
         if remove_background:
             rendervar = {k: v[is_fg] for k, v in rendervar.items()}
         scene_data.append(rendervar)

train.py
@@ -27,7 +27,7 @@ def mask_dataset(path_to_im, path_to_mask):
     seg = torch.tensor(seg).float()
     seg_col = torch.stack((seg, torch.zeros_like(seg), 1 - seg))
     # TODO: There must be a better way to do the masking
-    segmented_image[seg == 0.0] = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float32)
+    segmented_image[seg == 0.0] = torch.tensor([1.0, 1.0, 1.0], dtype=torch.float32)
     segmented_image = torch.permute(segmented_image, (2, 0, 1))
     return im, seg_col, segmented_image
 
@@ -35,28 +35,22 @@ def mask_dataset(path_to_im, path_to_mask):
 def get_dataset(t, md, seq, data_dir: str):
     dataset = []
     print("Preprocessing")
+    background_color = torch.ones(3)
     for c in tqdm(range(len(md['fn'][t]))):
         w, h, k, w2c = md['w'], md['h'], md['k'][t][c], md['w2c'][t][c]
-        cam = setup_camera(w, h, k, w2c, near=1.0, far=100)
+        cam = setup_camera(w, h, k, w2c, near=1.0, far=100, bg=background_color)
         fn = md['fn'][t][c]
-        path_to_im = f"{data_dir}/{seq}/ims/{fn}"
-        np_im = np.array(copy.deepcopy(Image.open(path_to_im)))
-        # im = torch.tensor(np_im).float().permute(2, 0, 1) / 255
-        # segmented_image = torch.tensor(np_im).float() / 255
-        path_to_mask = f"{data_dir}/{seq}/seg/{fn.replace('.jpg', '.png')}"
-        # seg = np.array(copy.deepcopy(Image.open(path_to_mask))).astype(
-        #     np.float32)
-        # seg = torch.tensor(seg).float()
-        # seg_col = torch.stack((seg, torch.zeros_like(seg), 1 - seg))
-        # # TODO: There must be a better way to do the masking
-        # segmented_image[seg == 0.0] = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float32)
-        # segmented_image = torch.permute(segmented_image, (2, 0, 1))
-        # Note: original code, transferred all image tensors to cuda, but this makes you run out of memory if the resolution is
-        # high, so I had to change it. The tradeoff is that now the tensors need to be moved every time before computation.
-        # dataset.append({'cam': cam, 'im': path_to_im, 'seg': seg_col, 'id': c, 'seg_im': segmented_image})
-        # im, seg_col, segmented_image = mask_dataset(path_to_im, path_to_mask)
-        # dataset.append({'cam': cam, 'im': im, 'seg': seg_col, 'id': c, 'seg_im': segmented_image})
-        dataset.append({'cam': cam, 'im': path_to_im, 'id': c, 'seg_im': path_to_mask})
+        np_im = np.array(copy.deepcopy(Image.open(f"{data_dir}/{seq}/ims/{fn}")))
+        im = torch.tensor(np_im).float().cuda().permute(2, 0, 1) / 255
+        segmented_image = torch.tensor(np_im).float() / 255
+        seg = np.array(copy.deepcopy(Image.open(f"{data_dir}/{seq}/seg/{fn.replace('.jpg', '.png')}"))).astype(
+            np.float32)
+        seg = torch.tensor(seg).float().cuda()
+        seg_col = torch.stack((seg, torch.zeros_like(seg), 1 - seg))
+        # TODO: There must be a better way to do the masking
+        segmented_image[seg == 0.0] = torch.tensor(background_color, dtype=torch.float32)
+        segmented_image = torch.permute(segmented_image, (2, 0, 1)).cuda()
+        dataset.append({'cam': cam, 'im': im, 'seg': seg_col, 'id': c, 'seg_im': segmented_image})
     return dataset
 
 
@@ -122,15 +116,18 @@ def get_loss(params, curr_data, variables, is_initial_timestep, use_entropy_loss
 
     rendervar = params2rendervar(params)
     rendervar['means2D'].retain_grad()
-    im, radius, _, = Renderer(raster_settings=curr_data['cam'])(**rendervar)
+    im, radius, depth_buffer = Renderer(raster_settings=curr_data['cam'])(**rendervar)
     curr_id = curr_data['id']
     im = torch.exp(params['cam_m'][curr_id])[:, None, None] * im + params['cam_c'][curr_id][:, None, None]
     # losses['im'] = 0.8 * l1_loss_v1(im, curr_data['seg_im']) + 0.2 * (1.0 - calc_ssim(im, curr_data['seg_im']))
-    _, seg_colors, seg_im = mask_dataset(curr_data["im"], curr_data["seg_im"])
-    seg_colors = seg_colors.cuda()
+    # _, seg_colors, seg_im = mask_dataset(curr_data["im"], curr_data["seg_im"])
+    seg_im = curr_data["seg_im"]
+    # seg_colors = curr_data["seg_colors"]
     seg_im = seg_im.cuda()
 
-    losses['im'] = l1_loss_masked(im, seg_im, seg_colors[0, :])
+    # losses['im'] = l1_loss_masked(im, seg_im, curr_data['seg'][0, :])
+    losses['im'] = 0.8 * l1_loss_masked(im, seg_im, curr_data['seg'][0, :]) + 0.2 * (
+            1.0 - calc_ssim(im, seg_im))
     # losses['im'] = l2_loss_masked(im, curr_data['seg_im'], curr_data['seg'][0, :])
 
     variables['means2D'] = rendervar['means2D']  # Gradient only accum from colour render for densification
@@ -169,7 +166,7 @@ def get_loss(params, curr_data, variables, is_initial_timestep, use_entropy_loss
         losses['soft_col_cons'] = l1_loss_v2(params['rgb_colors'], variables["prev_col"])
 
     # Losses for torus and pacnerf torus
-    loss_weights = {'im': 50, 'seg': 3.0, 'rigid': 1000.0, 'rot': 0.0, 'iso': 700.0, 'floor': 2.0, 'bg': 20.0,
+    loss_weights = {'im': 1.0, 'seg': 1.0, 'rigid': 1000.0, 'rot': 0.0, 'iso': 700.0, 'floor': 2.0, 'bg': 20.0,
                     'soft_col_cons': 0.01}
 
     wandb.log(losses)
@@ -240,11 +237,15 @@ def initialize_post_first_timestep(params, variables, optimizer, num_knn=20):
 
 def report_progress(params, data, i, progress_bar, every_i=100):
     if i % every_i == 0:
+        # if i > 500 and i % 2 == 0:
+        #     data['cam'].bg[0] = 1.0
+        #     data['cam'].bg[1] = 1.0
+        #     data['cam'].bg[2] = 1.0
         im, _, _, = Renderer(raster_settings=data['cam'])(**params2rendervar(params))
         wandb.log({"renders": wandb.Image(im)})
         curr_id = data['id']
         im = torch.exp(params['cam_m'][curr_id])[:, None, None] * im + params['cam_c'][curr_id][:, None, None]
-        data_im = load_im(data['im'])[0].cuda()
+        data_im = data['im']
         psnr = calc_psnr(im, data_im).mean()
         wandb.log({"PSNR": psnr})
         progress_bar.set_postfix({"train img 0 PSNR": f"{psnr:.{7}f}"})
@@ -288,9 +289,9 @@ def train(seq, exp, args: argparse.Namespace):
                 report_progress(params, dataset[0], i, progress_bar)
                 if is_initial_timestep:
                     params, variables = densify(params, variables, optimizer, i)
-                    if i == num_iter_per_timestep - 1:
-                        # params, variables = remove_transparent(params, variables, optimizer, remove_threshold=0.997)
-                        params, variables = poisson_subsample(params, variables, optimizer, target=12000)
+                    # if i == num_iter_per_timestep - 1:
+                    #     # params, variables = remove_transparent(params, variables, optimizer, remove_threshold=0.998)
+                    #     params, variables = poisson_subsample(params, variables, optimizer, target=12000)
                 optimizer.step()
                 optimizer.zero_grad(set_to_none=True)
             if i == num_iter_per_timestep - 1: